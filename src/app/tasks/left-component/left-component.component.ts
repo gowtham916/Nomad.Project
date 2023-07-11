6bd939ca25ed06938e2666569fd47be7d623f8df
Gowtham <gowthamkukku9@gmail.com>
2023-07-12 00:07:15 +0530

    Updated files

---
+++ src/app/tasks/left-component/left-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-component',
+  templateUrl: './left-component.component.html',
+  styleUrls: ['./left-component.component.css']
+})
+export class LeftComponentComponent {
+
+}
